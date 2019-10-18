8f54f023a7ce1e535fb6ae7fbd7af791622c6f87
Pavel Anihimovsky <p.anihimovsky@a1qa.com>
2019-10-18 15:39:25 +0300

    #14 Fix issues with default public constructor

--- Aquality.Selenium.Core/src/Aquality.Selenium.Core/Applications/ServiceProviderContainer.cs
+++ Aquality.Selenium.Core/src/Aquality.Selenium.Core/Applications/ServiceProviderContainer.cs
@@ -6,5 +6,9 @@ namespace Aquality.Selenium.Core.Applications
     public class ServiceProviderContainer
     {
         protected static readonly ThreadLocal<IServiceProvider> ServiceProviderInstanceHolder = new ThreadLocal<IServiceProvider>();
+
+        protected ServiceProviderContainer()
+        {
+        }
     }
 }
