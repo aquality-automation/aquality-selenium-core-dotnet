68d947188a25a7c19f1038483612e1af27c7ef25
Pavel Anihimovsky <pavel.anihimovsky@gmail.com>
2020-05-05 18:27:44 +0300

    Add tests for ActionRetrier and ElementActionRetrier

---
+++ Aquality.Selenium.Core/tests/Aquality.Selenium.Core.Tests/Utilities/ActionRetrierTests.cs
@@ -0,0 +1,103 @@
+﻿using Aquality.Selenium.Core.Utilities;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Aquality.Selenium.Core.Tests.Utilities
+{
+    public class ActionRetrierTests : RetrierTests
+    {
+        private IEnumerable<Type> HandledExceptions => new List<Type> { typeof(InvalidOperationException) };
+        
+        private IActionRetrier ActionRetrier => new ActionRetrier(RertyConfiguration);
+
+        [Test]
+        public void Retrier_ShouldWork_OnceIfMethodSucceeded()
+        {
+            Retrier_ShouldWork_OnceIfMethodSucceeded(() => ActionRetrier.DoWithRetry(() => Console.WriteLine(1), new List<Type>()));
+        }
+
+        [Test]
+        public void Retrier_ShouldWork_OnceIfMethodSucceeded_WithReturnValue()
+        {
+            Retrier_ShouldWork_OnceIfMethodSucceeded(() => ActionRetrier.DoWithRetry(() => string.Empty, new List<Type>()));
+        }
+
+        [Test]
+        public void Retrier_ShouldWait_PollingTimeBetweenMethodCalls()
+        {
+            var throwException = true;
+            Retrier_ShouldWait_PollingIntervalBetweenMethodsCall(() =>
+                    ActionRetrier.DoWithRetry(() => {
+                        if (throwException)
+                        {
+                            throwException = false;
+                            throw new InvalidOperationException();
+                        }
+                    }, HandledExceptions));
+        }
+
+        [Test]
+        public void Retrier_ShouldWait_PollingTimeBetweenMethodCalls_WithReturnValue()
+        {
+            var throwException = true;
+            Retrier_ShouldWait_PollingIntervalBetweenMethodsCall(() =>
+                    ActionRetrier.DoWithRetry(() => {
+                        if (throwException)
+                        {
+                            throwException = false;
+                            throw new InvalidOperationException();
+                        }
+                        return string.Empty;
+                    }, HandledExceptions));
+        }
+
+        [Test]
+        public void Retrier_ShouldThrow_UnhandledException()
+        {
+            Assert.Throws<InvalidOperationException>(() => ActionRetrier.DoWithRetry(() => {
+                throw new InvalidOperationException();
+                Console.WriteLine(1);
+            }, new List<Type>()));
+        }
+
+        [Test]
+        public void Retrier_ShouldThrow_UnhandledException_WithReturnValue()
+        {
+            Assert.Throws<InvalidOperationException>(() => ActionRetrier.DoWithRetry(() => {
+                throw new InvalidOperationException();
+                return string.Empty;
+            }, new List<Type>()));
+        }
+
+        [Test]
+        public void Retrier_ShouldWork_CorrectTimes()
+        {
+            var actualAttempts = 0;
+            Retrier_ShouldWork_CorrectTimes(typeof(InvalidOperationException), ref actualAttempts, () =>
+                    ActionRetrier.DoWithRetry(() => {
+                        Logger.Info($"current attempt is {actualAttempts++}");
+                        throw new InvalidOperationException();
+                    }, HandledExceptions));
+        }
+
+        [Test]
+        public void Retrier_ShouldWork_CorrectTimes_WithReturnValue()
+        {
+            var actualAttempts = 0;
+            Retrier_ShouldWork_CorrectTimes(typeof(InvalidOperationException), ref actualAttempts, () =>
+                    ActionRetrier.DoWithRetry(() => {
+                        Logger.Info($"current attempt is {actualAttempts++}");
+                        throw new InvalidOperationException();
+                        return string.Empty;
+                    }, HandledExceptions));
+        }
+
+        [Test]
+        public void Retrier_Should_ReturnValue()
+        {
+            var returnValue = string.Empty;
+            Assert.AreEqual(returnValue, ActionRetrier.DoWithRetry(() => returnValue, new List<Type>()), "Retrier should return value");
+        }
+    }
+}
---
+++ Aquality.Selenium.Core/tests/Aquality.Selenium.Core.Tests/Utilities/RetrierTests.cs
@@ -0,0 +1,62 @@
+﻿using Aquality.Selenium.Core.Configurations;
+using Aquality.Selenium.Core.Logging;
+using Aquality.Selenium.Core.Tests.Applications.Browser;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using System;
+
+namespace Aquality.Selenium.Core.Tests.Utilities
+{
+    public abstract class RetrierTests
+    {
+        protected const int ACCURACY = 100;
+
+        protected Logger Logger => AqualityServices.ServiceProvider.GetRequiredService<Logger>();
+
+        protected IRetryConfiguration RertyConfiguration => AqualityServices.ServiceProvider.GetRequiredService<IRetryConfiguration>();
+        
+        protected int PollingInterval => RertyConfiguration.PollingInterval.Milliseconds;
+
+        protected int RetriesCount => RertyConfiguration.Number;
+
+        protected void Retrier_ShouldWork_OnceIfMethodSucceeded(Action action)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            action();
+            watch.Stop();
+            var duration = watch.ElapsedMilliseconds;
+
+            Assert.IsTrue(duration < PollingInterval,
+                $"Duration '{duration}' should be less that pollingInterval '{PollingInterval}'");
+        }
+
+        protected void Retrier_ShouldWait_PollingIntervalBetweenMethodsCall(Action action)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            action();
+            watch.Stop();
+            var duration = watch.ElapsedMilliseconds;
+
+            Assert.IsTrue(duration >= PollingInterval, 
+                $"Duration '{duration}' should be more than polling interval '{PollingInterval}'");
+
+            var doubledAccuracyPollingInterval = 2 * PollingInterval + ACCURACY;
+
+            Assert.IsTrue(duration <= doubledAccuracyPollingInterval, 
+                $"Duration '{duration}' should be less than doubled polling interval '{doubledAccuracyPollingInterval}'");
+        }
+
+        protected void Retrier_ShouldWork_CorrectTimes(Type handledException, ref int actualAttempts, Action action)
+        {
+            try
+            {
+                action();
+            }
+            catch (Exception e)
+            {
+                Assert.IsTrue(handledException.IsAssignableFrom(e.GetType()));
+            }
+            Assert.AreEqual(actualAttempts, RetriesCount + 1, "actual attempts count is not match to expected");
+        }
+    }
+}
