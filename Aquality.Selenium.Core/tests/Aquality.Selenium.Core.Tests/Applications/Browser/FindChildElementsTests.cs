26a3bc74958fd38c3c47c746c0c99ce557031a61
Alaksiej Mialeška <mialeszkaalaksiej@gmail.com>
2020-05-22 19:31:45 +0300

    Implement XPath extraction logic and add tests

--- Aquality.Selenium.Core/tests/Aquality.Selenium.Core.Tests/Applications/Browser/FindChildElementsTests.cs
+++ Aquality.Selenium.Core/tests/Aquality.Selenium.Core.Tests/Applications/Browser/FindChildElementsTests.cs
@@ -9,40 +9,67 @@ namespace Aquality.Selenium.Core.Tests.Applications.Browser
 {
     public class FindChildElementsTests : FindElementsTests
     {
-        private readonly Label customParent = new Label(By.XPath("//div[contains(@class,'figure')][1]"), "custom parent", ElementState.ExistsInAnyState);
+        private readonly Label customParent = new Label(By.XPath("//div[contains(@class,'figure')]"), 
+            "custom parent", ElementState.ExistsInAnyState);
         protected override By HiddenElementsLoc => By.XPath(".//h5");
         protected override By DisplayedElementsLoc => By.XPath(".//img[@alt='User Avatar']");
         protected override By NotExistElementLoc => By.XPath(".//div[@class='testtest']");
 
-        protected override IList<T> FindElements<T>(By locator, string name = null, ElementSupplier<T> supplier = null, ElementsCount expectedCount = ElementsCount.Any, ElementState state = ElementState.Displayed)
+        private static readonly By[] SupportedLocators = new By[]
+        {
+            By.XPath("//img"),
+            By.XPath(".//img"),
+            By.TagName("img")
+        };
+
+        protected override IList<T> FindElements<T>(By locator, string name = null, ElementSupplier<T> supplier = null, 
+            ElementsCount expectedCount = ElementsCount.Any, ElementState state = ElementState.Displayed)
         {
             return ParentElement.FindChildElements(locator, name, supplier, expectedCount, state);
         }
 
         [Test]
-        public void Should_GetCorrectNumberOfChilds_ForRelativeChildLocator()
-        {
-            var expectedCount = 1;
-            var elementsCount = ElementFactory.FindChildElements<Label>(customParent, DisplayedElementsLoc).Count;
-            Assert.AreEqual(expectedCount, elementsCount, $"Elements count for relative locator should be {expectedCount}");
-        }
-
-        [Test]
-        public void Should_GetCorrectNumberOfChilds_ForAbsoluteChildLocator()
+        public void Should_GetCorrectNumberOfChilds_ForRelativeChildLocator(
+            [ValueSource(nameof(SupportedLocators))] By childRelativeLocator)
         {
             var expectedCount = 3;
-            var elementsCount = ElementFactory.FindChildElements<Label>(customParent, base.DisplayedElementsLoc).Count;
-            Assert.AreEqual(expectedCount, elementsCount, $"Elements count for absolute locator should be {expectedCount}");
+            var elementsCount = ElementFactory.FindChildElements<Label>(customParent, childRelativeLocator).Count;
+            Assert.AreEqual(expectedCount, elementsCount, 
+                $"Elements count for relative locator [{childRelativeLocator}] should be {expectedCount}");
         }
 
         [Test]
-        public void Should_SetWorkableLocators_ToChildElements()
+        public void Should_SetWorkableLocators_ToChildElements([ValueSource(nameof(SupportedLocators))] By childRelativeLocator)
         {
-            var foundElements = ElementFactory.FindChildElements<Label>(customParent, base.DisplayedElementsLoc);
+            var foundElements = ElementFactory.FindChildElements<Label>(customParent, childRelativeLocator);
             Assert.DoesNotThrow(() =>
             {
                 foundElements.Select(element => element.GetElement()).ToList();
             });
+        }
+
+        [Test]
+        public void Should_SetXPathLocator_InFindChildElement_IfBothParentAndChildHaveSupportedLocators(
+            [ValueSource(nameof(SupportedLocators))] By childRelativeLocator)
+        {
+            var childLocatorString = customParent.FindChildElement<Label>(childRelativeLocator).Locator.ToString();
+            CheckChildLocatorIsXpathAndStartsFromParent(childLocatorString);
+        }
+
+        [Test]
+        public void Should_SetXPathLocator_InFindChildElements_IfBothParentAndChildHaveSupportedLocators(
+            [ValueSource(nameof(SupportedLocators))] By childRelativeLocator)
+        {
+            var childLocatorStrings = customParent.FindChildElements<Label>(childRelativeLocator)
+                .Select(child => child.Locator.ToString())
+                .ToList();
+            Assert.Multiple(() => childLocatorStrings.ForEach(CheckChildLocatorIsXpathAndStartsFromParent));            
+        }
+
+        private void CheckChildLocatorIsXpathAndStartsFromParent(string childLocatorString)
+        {
+            StringAssert.StartsWith("By.XPath", childLocatorString);
+            StringAssert.Contains(customParent.Locator.ToString(), childLocatorString);
         }
     }
 }
