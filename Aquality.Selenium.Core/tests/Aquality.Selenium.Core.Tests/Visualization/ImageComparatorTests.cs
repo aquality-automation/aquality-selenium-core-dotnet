07f5ef60f90ab2945855eeeb335d6a20da945b95
Aleksey2 Meleshko <a2.meleshko@a1qa.com>
2023-02-20 20:46:09 +0100

    Fix image comparator test

--- Aquality.Selenium.Core/tests/Aquality.Selenium.Core.Tests/Visualization/ImageComparatorTests.cs
+++ Aquality.Selenium.Core/tests/Aquality.Selenium.Core.Tests/Visualization/ImageComparatorTests.cs
@@ -71,8 +71,8 @@ namespace Aquality.Selenium.Core.Tests.Visualization
             Assert.Multiple(() =>
             {
                 Assert.That(ImageComparator.PercentageDifference(firstImage, secondImage, threshold: 0), Is.Not.EqualTo(0));
-                Assert.That(ImageComparator.PercentageDifference(firstImage, secondImage, threshold: 0.2f), Is.AtMost(0.2));
-                Assert.That(ImageComparator.PercentageDifference(firstImage, secondImage, threshold: 0.4f), Is.AtMost(0.15));
+                Assert.That(ImageComparator.PercentageDifference(firstImage, secondImage, threshold: 0.2f), Is.AtMost(0.3));
+                Assert.That(ImageComparator.PercentageDifference(firstImage, secondImage, threshold: 0.4f), Is.AtMost(0.2));
                 Assert.That(ImageComparator.PercentageDifference(firstImage, secondImage, threshold: 0.6f), Is.AtMost(0.1));
                 Assert.That(ImageComparator.PercentageDifference(firstImage, secondImage, threshold: 0.8f), Is.EqualTo(0));
             });
