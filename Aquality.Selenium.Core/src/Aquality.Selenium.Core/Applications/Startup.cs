95eec73a69349d6cc66152080a987360a2154175
Aleksey Meleshko <a2.meleshko@a1qa.com>
2026-07-20 16:27:17 +0200

    fix assembly resolution in Startup

--- Aquality.Selenium.Core/src/Aquality.Selenium.Core/Applications/Startup.cs
+++ Aquality.Selenium.Core/src/Aquality.Selenium.Core/Applications/Startup.cs
@@ -65,14 +65,22 @@ namespace Aquality.Selenium.Core.Applications
                 var profileNameFromEnvironment = EnvironmentConfiguration.GetVariable("profile");
                 var settingsProfile = string.IsNullOrEmpty(profileNameFromEnvironment) ? "settings.json" : $"settings.{profileNameFromEnvironment}.json";
                 Logger.Instance.Debug($"Get settings from: {settingsProfile}");
+                var embeddedResourceName = $"Resources.{settingsProfile}";
 
                 var jsonFile = FileReader.IsResourceFileExist(settingsProfile)
                     ? new JsonSettingsFile(settingsProfile)
-                    : new JsonSettingsFile($"Resources.{settingsProfile}", Assembly.GetCallingAssembly());
+                    : new JsonSettingsFile(embeddedResourceName, ResolveAssembly(Assembly.GetCallingAssembly(), embeddedResourceName));
                 return jsonFile;
             }
 
             return settingsFile;
+        }
+
+        private static Assembly ResolveAssembly(Assembly assembly, string embeddedResourceName)
+        {
+            return Array.Exists(assembly.GetManifestResourceNames(), name => name.Contains(embeddedResourceName))
+                ? assembly
+                : Assembly.GetExecutingAssembly();
         }
     }
 }
